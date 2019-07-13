9ae1cf077716e5ca8f7e04a364fe267891b5854f
kylesmith41 <kylesmith@qualitytalk.com>
2019-07-13 14:01:56 -0400

    maybe

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,9 @@ import axios from 'axios';
 const config = {
   app_id:"1axqyK6k",
   client_id:"dj0yJmk9MTZOd01CZGpHZzJRJmQ9WVdrOU1XRjRjWGxMTm1zbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWEw",
-  client_secret:"b71a20e0637029c71762e9b475dbeb86f387f34f"
+  client_secret:"b71a20e0637029c71762e9b475dbeb86f387f34f",
+  redirect_uri:"https%3A%2F%2Fblahffblah.herokuapp.com",
+  code:"k8unzne"
 }
 
 const getAuthorization = () => {
@@ -22,17 +24,16 @@ const getAuthorization = () => {
 }
 
 const getToken = (r) => {
-  console.log('r', r);
-  // const {app_id, client_id, client_secret} = config;
-  // return (
-  //   axios.post("https://api.login.yahoo.com/oauth2/get_token", {
-  //
-  //   })
-  // )
+  const {app_id, client_id, client_secret, code, redirect_uri} = config;
+
+  return (
+    axios.post(`https://api.login.yahoo.com/oauth2/get_token?grant_type=authorization_code&redirect_uri=${redirect_uri}&code=${code}`)
+  )
 }
 
 const makeRequest = () => {
   getAuthorization();
+  getToken().then(r => console.log("response", r));
 };
 
 const Index = () => {
