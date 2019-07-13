05a50f6da2a84357ea283faf405c6595f0ea0982
kylesmith41 <kylesmith@qualitytalk.com>
2019-07-13 13:29:34 -0400

    maybe it works this time

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import axios from 'axios';
 
+const config = {
+  app_id:"1axqyK6k",
+  client_id:"dj0yJmk9MTZOd01CZGpHZzJRJmQ9WVdrOU1XRjRjWGxMTm1zbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmc3Y9MCZ4PWEw",
+  client_secret:"b71a20e0637029c71762e9b475dbeb86f387f34f"
+}
+
+const getAuthorization = () => {
+  const {app_id, client_id, client_secret} = config;
+  return (
+    axios
+      .put(
+        `https://api.login.yahoo.com/oauth2/request_auth?client_id=${client_id}&redirect_uri=https%3A%2F%2Fblahffblah.herokuapp.com/t%3A3000&response_type=code&language=en-us`
+      , { headers: {"Access-Control-Allow-Origin": "*"}})
+      .then(r => {
+        console.log("should have a code", r);
+        return r;
+      })
+  )
+}
+
+const getToken = (r) => {
+  console.log('r', r);
+  // const {app_id, client_id, client_secret} = config;
+  // return (
+  //   axios.post("https://api.login.yahoo.com/oauth2/get_token", {
+  //
+  //   })
+  // )
+}
+
 const makeRequest = () => {
-  axios
-    .get(
-      "https://fantasysports.yahooapis.com/fantasy/v2/users;use_login=1/games;game_keys=nfl/teams"
-    )
-    .then(r => {
-      console.log("r", r);
-    });
+  getAuthorization();
 };
 
 const Index = () => {
+  useEffect(makeRequest);
   return (
     <div>
       <p>Hello Next.js</p>
